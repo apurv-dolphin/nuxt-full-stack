f9296a8654f63691561109ef72c42980dd0e3c2a
apurv-dolphin <apurv.dolphinwebsolution@gmail.com>
2024-07-16 13:03:29 +0530

    remove a tag and replace with div.

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -3,15 +3,12 @@
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
-      <a
-        href="https://flowbite.com/"
-        class="flex items-center space-x-3 rtl:space-x-reverse"
-      >
+      <div class="flex items-center space-x-3 rtl:space-x-reverse">
         <span
           class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
           >Apurv</span
         >
-      </a>
+      </div>
       <button
         @click="toggleNav"
         type="button"
